b50addbd137ce7e58c1e2aed9d66983a2fd99fbe
ThreshMain <martinkos007@gmail.com>
2021-08-20 01:09:20 +0200

    Added few more packages and utilities

--- packages.nix
+++ packages.nix
@@ -12,9 +12,11 @@
       ranger lynx
       htop bpytop glances gotop nload iftop
       mutt
+      nyancat
+      hyperfine
       # Commands
       lsd home-manager neofetch screenfetch tmux openssh gnumake tldr nmap
-      most sshfs feh youtube-dl ffmpeg unzip zip
+      most sshfs feh youtube-dl ffmpeg unzip zip gcc killall asciinema
       # Utilities
       wget curl git pandoc cmake light blueman fzf autorandr cron bat lolcat
       gnupg libnotify libinput-gestures hunspell aspell gspell xclip
@@ -28,7 +30,7 @@
       # ======= GUI programms ======
       arandr alacritty dolphin gparted flameshot spotify pavucontrol
       gnome.gnome-power-manager firefox brave copyq lxappearance wireshark
-      gimp shotcut vlc blender font-manager
+      gimp shotcut vlc blender font-manager insomnia
       # Comunication
       discord
       # ===========================
@@ -43,6 +45,7 @@
       # Languages
       python2 jdk jdk8 php nodejs nodePackages.npm
       python37Full python37Packages.virtualenv python37Packages.pip python37Packages.setuptools
+      python39Full python39Packages.virtualenv python39Packages.pip python39Packages.setuptools
       # --------------------------
   ];
   nixpkgs.config = {
