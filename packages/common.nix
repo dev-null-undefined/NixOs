32fcec864ca41ee878430f34fc779f83f7f19a46
ThreshMain <martinkos007@gmail.com>
2021-09-23 12:58:06 +0200

    moved ranger and added dependency for image preview

--- packages/common.nix
+++ packages/common.nix
@@ -2,17 +2,23 @@
 
 {
   programs.gnupg.agent.enable = true;
-  
+
+  imports = [
+    ./ranger.nix
+  ];
+
   environment.systemPackages = with pkgs; [
     # TUIS
     vim_configurable neovim
     htop 
-    mc ranger
+    mc 
+    sl # :D
 
     # Commands
     neofetch cpufetch
     tmux openssh gnumake tldr nmap tree gcc
-    killall thefuck nix-diff nix-index traceroute
+    killall thefuck nix-diff nix-index traceroute pciutils
+    openssl
 
     # man page
     man man-pages
@@ -21,10 +27,10 @@
     wget curl git cmake gnupg lsof whois dnsutils file
 
     # Languages
-    jdk jdk8 php nodejs nodePackages.npm
-    python27Full python27Packages.virtualenv python27Packages.pip python27Packages.setuptools
-    python37Full python37Packages.virtualenv python37Packages.pip python37Packages.setuptools
-    python39Full python39Packages.virtualenv python39Packages.pip python39Packages.setuptools
+    jdk jdk8 php nodejs nodePackages.npm yarn
+    # python27Full python27Packages.virtualenv python27Packages.pip python27Packages.setuptools
+    # python37Full python37Packages.virtualenv python37Packages.pip python37Packages.setuptools
+    # python39Full python39Packages.virtualenv python39Packages.pip python39Packages.setuptools
     # --------------------------
   ];
    
---
+++ packages/ranger.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }:
+
+{
+  config = lib.mkMerge
+  [ { 
+       environment.systemPackages = with pkgs; [
+         ranger
+       ];
+    }
+    (lib.mkIf config.services.xserver.enable {
+      environment.systemPackages = with pkgs; [ 
+         python39Packages.ueberzug
+      ];
+    })
+  ];
+}
