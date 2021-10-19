aa1ab1a21439c9b6b246c043a2ec300606378e4f
ThreshMain <martinkos007@gmail.com>
2021-10-19 21:46:31 +0200

    Added rider and cleaned up the syntax

--- packages/gui/jetbrains.nix
+++ packages/gui/jetbrains.nix
@@ -2,14 +2,14 @@
 
 {
 
-  environment.systemPackages = with pkgs; [
-    jetbrains.idea-ultimate jetbrains.phpstorm jetbrains.jdk jetbrains.pycharm-professional jetbrains.webstorm
-      (pkgs.jetbrains.clion.overrideAttrs (old: rec{
-        version = "2021.2.2";
-        src = fetchurl {
-          url = "https://download.jetbrains.com/cpp/CLion-${version}.tar.gz";
-          sha256 = "0q9givi3w8q7kdi5y7g1pj4c7kb00a4v0fvry04zsahdq5lzkikh";
-        };
-      }))
+  environment.systemPackages = with pkgs.jetbrains; [
+    idea-ultimate
+    phpstorm
+    pycharm-professional
+    webstorm
+    clion
+    rider
+
+    jdk
   ];
 }
