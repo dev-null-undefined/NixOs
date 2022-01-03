19706817a4efe58e98fee91cb830e1ccbd49b6dc
ThreshMain <martinkos007@gmail.com>
2022-01-03 17:58:11 +0100

    Disabled docker on boot and added tui for managing images

--- packages/docker.nix
+++ packages/docker.nix
@@ -1,8 +1,12 @@
 { config, pkgs, lib, ... }:
 
 {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = false;
+  };
   environment.systemPackages = with pkgs; [
     docker-compose
+    lazydocker # tui for managing dockers
   ];
 }
