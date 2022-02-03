6af8f89dad0d5ce4439b1c913e70596e9d3b787a
ThreshMain <martinkos007@gmail.com>
2022-02-03 06:42:42 +0100

    Better formating of audio config

--- de/audio/pipewire.nix
+++ de/audio/pipewire.nix
@@ -4,8 +4,11 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
