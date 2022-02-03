d5134d4f8fa2a1a426aa42a01e39c0a5d53da2c9
ThreshMain <martinkos007@gmail.com>
2022-02-03 06:41:04 +0100

    Moved to polymc instead of multimc

--- packages/gui/games.nix
+++ packages/gui/games.nix
@@ -12,15 +12,27 @@ in {
   };
 
   environment.systemPackages = with pkgs; [
-    (stable.multimc.overrideAttrs (old: rec{
+    # We will remember you multimc <3
+    #(stable.multimc.overrideAttrs (old: rec{
+    #  src = fetchFromGitHub {
+    #    owner = "AfoninZ";
+    #    repo = "MultiMC5-Cracked";
+    #    rev = "9069e9c9d0b7951c310fdcc8bdc70ebc422a7634";
+    #    sha256 = "0nyf3gm0r3k3dbi4bd21g2lj840nkhz43pnlfjcz199y2f3zbdbl";
+    #    fetchSubmodules = true;
+    #  };
+    #})) 
+    (polymc.overrideAttrs (old: rec{
       src = fetchFromGitHub {
-        owner = "AfoninZ";
-        repo = "MultiMC5-Cracked";
-        rev = "9069e9c9d0b7951c310fdcc8bdc70ebc422a7634";
-        sha256 = "0nyf3gm0r3k3dbi4bd21g2lj840nkhz43pnlfjcz199y2f3zbdbl";
+        owner = "lebestnoob";
+        repo = "PolyMC-Offline";
+        rev = "dcb9e5c153dfcac3d56f7266a6a9dadbdcb26e86";
+        sha256 = "sha256-8OYIw+IIWxfmtWgD7Ft64PfiOkdFzQ0GD09ifgdQwfQ=";
         fetchSubmodules = true;
       };
-    })) 
+      patches = [ ];
+    }))
+    
     vitetris
     lutris
     gnome.zenity
