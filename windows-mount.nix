9c3b3e148afd0676d35fb0d49eab99b12428f152
ThreshMain <martinkos007@gmail.com>
2022-02-05 19:47:52 +0100

    Moved windows mount settings to custom file and created ntfs-3g suid wrapper

---
+++ windows-mount.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  security.wrappers."mount.ntfs3g-suid" =
+    { 
+        setuid = true;
+        owner = "root";
+        group = "root";
+        source = "${pkgs.ntfs3g.out}/bin/ntfs-3g";
+  };
+
+  environment.systemPackages = with pkgs; [
+    ntfs3g
+  ];  
+
+  fileSystems."/mnt/windows" = 
+  {
+    device = "/dev/disk/by-uuid/905CDBC35CDBA1F0";
+    fsType = "ntfs3g-suid";
+    options = [ "user" "rw" "utf8" "noauto" "umask=000" "uid=1000" ];
+  };
+}
