eca4f484f83b43bb17f8e8f9f61d680c4b4230be
ThreshMain <martinkos007@gmail.com>
2022-02-14 11:11:52 +0100

    Virt manager from stable channel

--- packages/gui/virt-manager.nix
+++ packages/gui/virt-manager.nix
@@ -1,7 +1,9 @@
 { pkgs,... }:
 
-{
+let
+    stable = import <nixos-stable> { config = { allowUnfree = true; }; };
+in {
   virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true;
-  environment.systemPackages = with pkgs; [ virt-manager ];
+  environment.systemPackages = with pkgs; [ stable.virt-manager ];
 }
