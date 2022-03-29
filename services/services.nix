23e844a07f5342b373915688a7783443326e1498
ThreshMain <martinkos007@gmail.com>
2022-03-29 05:37:40 +0200

    enable openvpn and nginx service

--- services/services.nix
+++ services/services.nix
@@ -4,8 +4,11 @@
     imports =
       [
         ./ssh.nix
+        ./nginx.nix
        # ./wireguard.nix
+       # ./wireguard-client.nix
         ./nextcloud.nix
         ./mariadb.nix
+        ./openvpn.nix
       ];
 }
