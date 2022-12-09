0b3b5befe5daf9fb53db57dcd7c9f996f4b10d2d
dev-null-undefined <martinkos007@gmail.com>
2022-12-10 00:17:38 +0100

    pkg: using only basic discord

--- packages/gui/discord.nix
+++ packages/gui/discord.nix
@@ -1,14 +1,15 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    (master.discord.override {
-      nss = nss_latest;
-      withOpenASAR = true;
-    })
-    # discord-ptb
-    # discord-canary
-    dev-null.webcord
-    # betterdiscord-installer
-  ];
+  environment.systemPackages = with pkgs;
+    [
+      (master.discord.override {
+        nss = nss_latest;
+        withOpenASAR = true;
+      })
+      # discord-ptb
+      # discord-canary
+      #dev-null.webcord
+      # betterdiscord-installer
+    ];
 }
