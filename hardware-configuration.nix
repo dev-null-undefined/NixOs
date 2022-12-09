ac44631d6bdb33390d8aa003df0cd7911258052f
dev-null-undefined <martinkos007@gmail.com>
2022-12-10 00:42:50 +0100

    yubikey 2fa, full disk encryption and btrfs

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -1,11 +1,11 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix") ./windows-mount.nix ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    ./hardware-partitions.nix
+  ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" ];
-  boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" "v4l2loopback" "ec_sys" ];
   boot.extraModulePackages = with config.boot.kernelPackages;
     [ v4l2loopback.out ];
@@ -36,22 +36,6 @@
   services.logind.lidSwitch = "ignore";
   services.logind.lidSwitchDocked = "ignore";
   services.logind.extraConfig = "HandleLidSwitch=ignore";
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/feee7f36-8bcc-4d80-8626-086896503105";
-    fsType = "ext4";
-  };
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/8d7d4e81-0760-4650-b847-5aa472286c09";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/DA0A-EAB3";
-    fsType = "vfat";
-  };
-
-  swapDevices = [{ device = "/dev/nvme1n1p2"; }];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode =
