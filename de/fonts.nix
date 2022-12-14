f9c63b1b6d9cee101f932bedbdc52900a5d22e5b
dev-null-undefined <martinkos007@gmail.com>
2022-12-14 23:35:02 +0100

    font config rewrite with default settings

--- de/fonts.nix
+++ de/fonts.nix
@@ -1,20 +1,41 @@
 { pkgs, ... }: {
-  fonts.fonts = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    master.fira-code
-    master.fira-code-symbols
-    carlito
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-    font-awesome_4
-    font-awesome
-    meslo-lgs-nf
-    jetbrains-mono
-    overpass
-    monocraft
-  ];
+  fonts = {
+    fonts = with pkgs; [
+      ubuntu_font_family
+      
+      noto-fonts
+      noto-fonts-cjk
+
+      twitter-color-emoji
+      noto-fonts-emoji
+
+      liberation_ttf
+
+      fira-code
+      fira-code-symbols
+
+      dejavu_fonts
+      carlito
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
+      font-awesome_4
+      font-awesome
+      
+      meslo-lgs-nf
+      jetbrains-mono
+      
+      overpass
+      monocraft
+    ];
+
+    fontDir.enable = true;
+    
+    enableDefaultFonts = true;
+    fontconfig = {
+      defaultFonts = {
+        emoji = [ "Noto Color Emoji" "Twitter Color Emoji" ];
+      };
+    };
+  };
 }
