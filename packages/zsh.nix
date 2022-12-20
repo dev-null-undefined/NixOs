0d49affb3534147339975ecdff31136e4c9c70d6
dev-null-undefined <martinkos007@gmail.com>
2022-12-20 01:28:46 +0100

    zsh nix autocompletion and other completions

--- packages/zsh.nix
+++ packages/zsh.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 
 {
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+  };
 
   environment.systemPackages = with pkgs; [
     zsh
