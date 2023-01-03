d0ac1443b619bb6ef1ff7b534ccfe43b908dc36d
dev-null-undefined <martinkos007@gmail.com>
2023-01-03 17:28:13 +0100

    nginx home page visualsorting

--- hosts/common/services/nginx.nix
+++ hosts/common/services/nginx.nix
@@ -1,10 +1,16 @@
 { pkgs, ... }:
-
-{
+let
+  visualSorting = pkgs.fetchFromGitHub {
+    owner = "dev-null-undefined";
+    repo = "VisualSorting";
+    rev = "2fdedd6bab68384536ece120ec940fde7b8a024a";
+    sha256 = "sha256-2ZeHGvGSrbyuppjzIsnkZTKi7mPXlJuLy9ksOnqeFrs=";
+  };
+in {
   services.nginx = {
     enable = true;
     virtualHosts."dev-null.me" = {
-      root = "/var/www";
+      root = visualSorting;
       locations."~ /\\.git".extraConfig = ''
         deny all;
       '';
