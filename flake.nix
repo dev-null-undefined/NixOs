59e824b093112612cf193d5bfec877efabda908e
dev-null-undefined <martinkos007@gmail.com>
2023-01-03 20:57:10 +0100

    few more nix tools and formatter settings

--- flake.nix
+++ flake.nix
@@ -15,69 +15,96 @@
     nixpkgs-dev-null.url = "github:dev-null-undefined/nixpkgs/master";
     nixpkgs-webcord.url = "github:dev-null-undefined/nixpkgs/webcord";
     nixpkgs-testing.url = "github:dev-null-undefined/nixpkgs/jetbrains-update";
+
+    # https://github.com/thiagokokada/nix-alien
+    # Run unpatched binaries on Nix/NixOS
+    nix-alien.url = "github:thiagokokada/nix-alien";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, nixpkgs-master, nixos-hardware
-    , dwm-dev-null, nixpkgs-dev-null, nixpkgs-webcord, nixpkgs-testing }@inputs:
-    let
-
-      mkPkgs = pkgs: overlays: system:
-        pkgs {
-          inherit system;
-          overlays = overlays;
-          config.allowUnfree = true;
-          config.permittedInsecurePackages = [ "electron-12.2.3" ];
-        };
-
-      mkOverlay =
-        { input ? inputs."nixpkgs-${name}", name, overlays ? [ ], system }:
-        (final: prev: ({ "${name}" = mkPkgs (import input) overlays system; }));
-
-      pkgs = system:
-        mkPkgs (import nixpkgs) [
-          (mkOverlay ({
-            inherit system;
-            name = "stable";
-          }))
-          (mkOverlay ({
-            inherit system;
-            name = "dev-null";
-          }))
-          (mkOverlay ({
-            inherit system;
-            name = "testing";
-          }))
-          (mkOverlay ({
-            inherit system;
-            name = "master";
-          }))
-          (mkOverlay ({
-            inherit system;
-            name = "webcord";
-          }))
-          (import ./pkgs)
-        ] system;
-
-      mkHost = { hostname, system ? "x86_64-linux", modules ? [ ] }: {
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-stable,
+    nixpkgs-master,
+    nixos-hardware,
+    dwm-dev-null,
+    nixpkgs-dev-null,
+    nixpkgs-webcord,
+    nixpkgs-testing,
+    nix-alien,
+  } @ inputs: let
+    mkPkgs = pkgs: overlays: system:
+      pkgs {
         inherit system;
-        pkgs = pkgs system;
-        specialArgs = { inherit inputs; };
-        modules = [
+        overlays = overlays;
+        config.allowUnfree = true;
+        config.permittedInsecurePackages = ["electron-12.2.3"];
+      };
+
+    mkOverlay = {
+      input ? inputs."nixpkgs-${name}",
+      name,
+      overlays ? [],
+      system,
+    }: (final: prev: {"${name}" = mkPkgs (import input) overlays system;});
+
+    pkgs = system:
+      mkPkgs (import nixpkgs) [
+        (mkOverlay {
+          inherit system;
+          name = "stable";
+        })
+        (mkOverlay {
+          inherit system;
+          name = "dev-null";
+        })
+        (mkOverlay {
+          inherit system;
+          name = "testing";
+        })
+        (mkOverlay {
+          inherit system;
+          name = "master";
+        })
+        (mkOverlay {
+          inherit system;
+          name = "webcord";
+        })
+        (import ./pkgs)
+      ]
+      system;
+
+    mkHost = {
+      hostname,
+      system ? "x86_64-linux",
+      modules ? [],
+    }: {
+      inherit system;
+      pkgs = pkgs system;
+      specialArgs = {inherit inputs;};
+      modules =
+        [
           ./modules/grub-savedefault.nix
           ./modules/hostname.nix
-          ({ config, lib, pkgs, ... }: {
+          ({
+            config,
+            lib,
+            pkgs,
+            ...
+          }: {
             inherit hostname;
             nix = {
               settings = {
-                experimental-features = [ "nix-command" "flakes" ];
+                experimental-features = ["nix-command" "flakes"];
                 auto-optimise-store = true;
                 keep-outputs = true;
                 keep-derivations = true;
               };
-              nixPath = lib.mapAttrsToList (k: v: "${k}=${v.to.path}")
+              nixPath =
+                lib.mapAttrsToList (k: v: "${k}=${v.to.path}")
                 config.nix.registry;
               registry =
-                nixpkgs.lib.mapAttrs (_: value: { flake = value; }) inputs;
+                nixpkgs.lib.mapAttrs (_: value: {flake = value;}) inputs;
               # Make use of latest `nix` to allow usage of `nix flake`s.
               package = pkgs.nix;
             };
@@ -86,20 +113,23 @@
           (./hosts + "/${hostname}/hardware-configuration.nix")
           (./hosts + "/${hostname}/default.nix")
           ./hosts/common/default.nix
-        ] ++ modules;
-      };
+        ]
+        ++ modules;
+    };
 
-      hostConfigs = [
-        { hostname = "idk"; }
-        {
-          hostname = "oracle-server";
-          system = "aarch64-linux";
-        }
-      ];
-    in {
-      nixosConfigurations = builtins.listToAttrs (builtins.map (config: {
+    hostConfigs = [
+      {hostname = "idk";}
+      {
+        hostname = "oracle-server";
+        system = "aarch64-linux";
+      }
+    ];
+  in {
+    nixosConfigurations = builtins.listToAttrs (builtins.map (config: {
         name = config.hostname;
         value = nixpkgs.lib.nixosSystem (mkHost config);
-      }) hostConfigs);
-    };
+      })
+      hostConfigs);
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+  };
 }
--- hosts/common/packages/development/languages/nix.nix
+++ hosts/common/packages/development/languages/nix.nix
@@ -1,10 +1,42 @@
-{ pkgs, config, ... }:
-
 {
-  environment.systemPackages = with pkgs; [
-    # lsp server for nix
-    nil
-    # Nix formatter
-    nixfmt
+  pkgs,
+  config,
+  inputs,
+  ...
+}: let
+  alien-pkgs = with inputs.nix-alien.packages.${config.nixpkgs.system}; [
+    nix-alien
+    nix-index-update
   ];
+in {
+  programs.nix-ld.enable = true;
+  environment.systemPackages = with pkgs;
+    [
+      # nix documentation
+      manix
+
+      # lsp server for nix
+      nil
+
+      # Linter
+      statix
+
+      # Nix formatter
+      nixfmt
+
+      # nix tools
+      nix-diff
+
+      # Fast searching for lib or package
+      nix-index
+
+      nix-direnv
+
+      # Build monitoring tool
+      nix-output-monitor
+
+      # dependency graphs in ranger like view
+      nix-tree
+    ]
+    ++ alien-pkgs;
 }
