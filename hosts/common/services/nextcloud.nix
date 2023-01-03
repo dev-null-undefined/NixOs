9de575613a9d11887f6112476060ad1a3e996cc8
dev-null-undefined <martinkos007@gmail.com>
2023-01-03 18:05:44 +0100

    nextcloud ACME certificate

--- hosts/common/services/nextcloud.nix
+++ hosts/common/services/nextcloud.nix
@@ -1,37 +1,51 @@
 { pkgs, ... }:
 
-{
-  imports = [ ./mariadb.nix ];
+let domain = "cloud.dev-null.me";
+in {
+  imports = [ ./mariadb.nix ./nginx.nix ];
 
   networking.firewall.allowedTCPPorts = [ 80 ];
-  services.nextcloud = {
-    package = pkgs.nextcloud25;
-    enable = true;
-    hostName = "cloud.dev-null.me";
-    https = true;
-    home = "/nextcloud";
-    caching = {
-      apcu = true;
-      redis = false;
-      memcached = true;
+  services = {
+    nextcloud = {
+      package = pkgs.nextcloud25;
+      enable = true;
+      hostName = domain;
+      https = true;
+      home = "/nextcloud";
+      caching = {
+        apcu = true;
+        redis = false;
+        memcached = true;
+      };
+      config = {
+        dbtype = "mysql";
+        dbname = "nextcloud";
+        dbuser = "nextcloud";
+        dbhost = "127.0.0.1";
+        dbport = 3306;
+        dbpassFile = "/var/nextcloud-db-pass";
+        adminpassFile = "/var/nextcloud-admin-pass";
+        extraTrustedDomains = [ "cloud.dev-null.me" ];
+      };
     };
-    config = {
-      dbtype = "mysql";
-      dbname = "nextcloud";
-      dbuser = "nextcloud";
-      dbhost = "127.0.0.1";
-      dbport = 3306;
-      dbpassFile = "/var/nextcloud-db-pass";
-      adminpassFile = "/var/nextcloud-admin-pass";
-      extraTrustedDomains = [ "cloud.dev-null.me" ];
+    mysql = {
+      ensureDatabases = [ "nextcloud" ];
+      ensureUsers = [{
+        name = "nextcloud";
+        ensurePermissions = { "nextcloud.*" = "ALL PRIVILEGES"; };
+      }];
+    };
+    nginx = {
+      # Setup Nextcloud virtual host to listen on ports
+      virtualHosts = {
+        ${domain} = {
+          ## Force HTTP redirect to HTTPS
+          forceSSL = true;
+          ## LetsEncrypt
+          enableACME = true;
+        };
+      };
     };
   };
 
-  services.mysql = {
-    ensureDatabases = [ "nextcloud" ];
-    ensureUsers = [{
-      name = "nextcloud";
-      ensurePermissions = { "nextcloud.*" = "ALL PRIVILEGES"; };
-    }];
-  };
 }
