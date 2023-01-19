0da2c4a212c13ac610602ba144bf6c0509f9a874
dev-null-undefined <martinkos007@gmail.com>
2023-01-19 01:36:02 +0100

    removed hostname alias from syncthing

--- hosts/common/services/syncthing.nix
+++ hosts/common/services/syncthing.nix
@@ -1,12 +1,7 @@
 {pkgs, ...}: let
   username = "martin";
-  domain = "sync.me";
 in {
   # default port is 8384
-  networking.extraHosts = ''
-    127.0.0.1 ${domain}
-  '';
-
   services.syncthing = {
     enable = true;
     user = username;
