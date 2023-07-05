f820600471e601c16bb81dae3fc3b1229ac4b638
dev-null-undefined <martinkos007@gmail.com>
2023-07-05 16:06:37 +0200

    added default password for martin user

--- modules/generated/nixos/users/martin.nix
+++ modules/generated/nixos/users/martin.nix
@@ -8,6 +8,7 @@
 in {
   users.users.martin = {
     isNormalUser = true;
+    hashedPassword = "$6$ZRbhOp8RsXm4CSqy$AtplJAXukQtMJk8cD/jEnWMyQaObP8x.kbhytD.4R4iLLJa3zHXNMzRTK1gYilfZNwU0g580/1s603ic/c.y..";
     extraGroups =
       ["wheel" "dialout" "disk"]
       ++ ifTheyExist [
