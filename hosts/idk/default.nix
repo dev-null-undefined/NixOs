b2b6da8af5a5fc7d818a48775a1572bf9150448c
dev-null-undefined <martinkos007@gmail.com>
2023-07-08 05:25:29 +0200

    enabled mathematica on idk

--- hosts/idk/default.nix
+++ hosts/idk/default.nix
@@ -21,10 +21,7 @@
       syncthing.enable = true;
       mariadb.enable = true;
     };
-    packages.gui = {
-      virtual-box.enable = lib.mkForce false;
-      mathematica.enable = lib.mkForce false;
-    };
+    packages.gui.virtual-box.enable = lib.mkForce false;
   };
 
   #networking.firewall.enable = false;
