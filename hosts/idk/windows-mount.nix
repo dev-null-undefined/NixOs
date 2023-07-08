2ecc35b0796b479c0bbf6ad355220212c24cd113
dev-null-undefined <martinkos007@gmail.com>
2023-07-08 05:25:50 +0200

    idk fixed windows disk ID

--- hosts/idk/windows-mount.nix
+++ hosts/idk/windows-mount.nix
@@ -9,7 +9,7 @@
   environment.systemPackages = with pkgs; [ntfs3g];
 
   fileSystems."/mnt/windows" = {
-    device = "/dev/disk/by-uuid/0E86E4D286E4BB7B";
+    device = "/dev/disk/by-uuid/9A844FFD844FDB01";
     fsType = "ntfs3g-suid";
     options = ["user" "rw" "utf8" "noauto" "umask=000" "uid=1000"];
   };
