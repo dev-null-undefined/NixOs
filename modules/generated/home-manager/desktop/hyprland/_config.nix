d34c5f4c261bd0bff3776f3cc1d44ff0840eef3b
dev-null-undefined <martinkos007@gmail.com>
2023-09-25 14:39:08 +0200

    hyprland jetbrains rules since java is an ass

--- modules/generated/home-manager/desktop/hyprland/_config.nix
+++ modules/generated/home-manager/desktop/hyprland/_config.nix
@@ -270,8 +270,8 @@
 
   # Jetbrains products
   # windowrulev2 = float,floating:0,class:^(jetbrains-.*),title:^(win.*)
-  # windowrulev2 = float,class:^(jetbrains-.*),title:^(Welcome to.*)
-  # windowrulev2 = center,class:^(jetbrains-.*),title:^(Replace All)$
+  windowrulev2 = float,class:^(jetbrains-.*),title:^(Welcome to.*)
+  windowrulev2 = center,class:^(jetbrains-.*)
   # windowrulev2 = forceinput,class:^(jetbrains-.*)
   # windowrulev2 = windowdance,class:^(jetbrains-.*) # allows IDE to move child windows
 ''
