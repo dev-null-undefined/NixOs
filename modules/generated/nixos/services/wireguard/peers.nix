2b3f8dbf2dfaa057b30395b96a25348d373fa66d
dev-null-undefined <martinkos007@gmail.com>
2023-10-13 13:05:38 +0200

    service wireguard: init

---
+++ modules/generated/nixos/services/wireguard/peers.nix
@@ -0,0 +1,10 @@
+{
+  networking.wireguard.interfaces.wg0.peers = [
+    # List of allowed peers.
+    {
+      # RPI home-assistant brnikov
+      publicKey = "IYujtBpTlBBZ2hzv6P6BDQqm9hOAizitkPN4YvnOpxE=";
+      allowedIPs = ["10.100.0.2/32"];
+    }
+  ];
+}
