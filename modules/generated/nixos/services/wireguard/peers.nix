4d4404f07c4297db189430a68b6629c9d97e0c5f
dev-null-undefined <martinkos007@gmail.com>
2023-10-13 14:04:38 +0200

    wireguard: persistentKeepalive update

--- modules/generated/nixos/services/wireguard/peers.nix
+++ modules/generated/nixos/services/wireguard/peers.nix
@@ -5,6 +5,7 @@
       # RPI home-assistant brnikov
       publicKey = "IYujtBpTlBBZ2hzv6P6BDQqm9hOAizitkPN4YvnOpxE=";
       allowedIPs = ["10.100.0.2/32"];
+      persistentKeepalive = 25;
     }
   ];
 }
