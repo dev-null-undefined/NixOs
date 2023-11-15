8a0be7d7f28d7d03eb110af567323af9b347410a
dev-null-undefined <martinkos007@gmail.com>
2023-11-15 01:31:54 +0100

    xhost to fix gparted on hyprland

--- modules/generated/home-manager/desktop/hyprland/_config.nix
+++ modules/generated/home-manager/desktop/hyprland/_config.nix
@@ -18,6 +18,7 @@
   exec-once = sleep 1 && syncthingtray
   exec-once = ${pkgs.libsForQt5.polkit-kde-agent.outPath}/libexec/polkit-kde-authentication-agent-1
   exec-once = ${pkgs.galaxy-buds-client.outPath}/bin/GalaxyBudsClient
+  exec-once = ${pkgs.xorg.xhost.outPath}/bin/xhost +
 
   # exec-once = swaylock --grace -1 # Enable this if you autostart hyprland without window manager
 
