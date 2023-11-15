834419a9a9b30183ec56465642c4e3e114283414
dev-null-undefined <martinkos007@gmail.com>
2023-11-16 00:35:29 +0100

    renamed lambda params to setisfy nix flake check

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
-_self: super: {
-  adi1090x-plymouth = super.callPackage ./adi1090x-plymouth {};
-  material-symbols = super.callPackage ./material-symbols {};
-  nomos-rebuild = super.callPackage ./nomos-rebuild {};
+final: prev: {
+  adi1090x-plymouth = prev.callPackage ./adi1090x-plymouth {};
+  material-symbols = prev.callPackage ./material-symbols {};
+  nomos-rebuild = prev.callPackage ./nomos-rebuild {};
 }
