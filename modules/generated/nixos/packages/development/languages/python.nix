aead769a1f7f5eb59467a3a7145942afde870e08
dev-null-undefined <martinkos007@gmail.com>
2023-12-13 01:35:44 +0100

    ueberzug use any python3

--- modules/generated/nixos/packages/development/languages/python.nix
+++ modules/generated/nixos/packages/development/languages/python.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     # Python
-    python39Packages.ueberzug
+    python3Packages.ueberzug
 
     (python3.withPackages
       (e: [e.matplotlib e.pygments e.numpy e.tkinter e.pandas]))
