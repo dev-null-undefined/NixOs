368660ac142d0121bfdfe207597184e0858787f3
dev-null-undefined <martinkos007@gmail.com>
2023-12-14 12:56:31 +0100

    idk host: reformat hardware-partitions

--- hosts/idk/hardware-partitions.nix
+++ hosts/idk/hardware-partitions.nix
@@ -3,57 +3,63 @@
 in {
   imports = [./windows-mount.nix];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = ["dm-snapshot" "vfat" "nls_cp437" "nls_iso8859-1" "usbhid"];
-  boot.supportedFilesystems = ["btrfs"];
+  boot = {
+    supportedFilesystems = ["btrfs"];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/${lvmRoot}";
-    fsType = "btrfs";
-    options = ["subvol=/root" "compress=zstd"];
+    initrd = {
+      availableKernelModules = ["xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod"];
+      kernelModules = ["dm-snapshot" "vfat" "nls_cp437" "nls_iso8859-1" "usbhid"];
+
+      luks.devices.root = {
+        device = "/dev/disk/by-uuid/2e0eeb5d-bd42-471b-93c0-768b12d9b66e";
+      };
+    };
+
+    loader = {
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+        useOSProber = true;
+        enableCryptodisk = true;
+        saveDefault = true;
+      };
+      efi.canTouchEfiVariables = true;
+    };
   };
 
-  fileSystems."/root/btrfs-top-lvl" = {
-    device = "/dev/disk/by-uuid/${lvmRoot}";
-    fsType = "btrfs";
-    options = ["subvol=/" "compress=zstd"];
-  };
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/${lvmRoot}";
+      fsType = "btrfs";
+      options = ["subvol=/root" "compress=zstd"];
+    };
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/${lvmRoot}";
-    fsType = "btrfs";
-    options = ["subvol=/home" "compress=zstd"];
-  };
+    "/root/btrfs-top-lvl" = {
+      device = "/dev/disk/by-uuid/${lvmRoot}";
+      fsType = "btrfs";
+      options = ["subvol=/" "compress=zstd"];
+    };
 
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/${lvmRoot}";
-    fsType = "btrfs";
-    options = ["subvol=/nix" "compress=zstd" "noatime"];
-  };
+    "/home" = {
+      device = "/dev/disk/by-uuid/${lvmRoot}";
+      fsType = "btrfs";
+      options = ["subvol=/home" "compress=zstd"];
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/D5CD-12F4";
-    fsType = "vfat";
+    "/nix" = {
+      device = "/dev/disk/by-uuid/${lvmRoot}";
+      fsType = "btrfs";
+      options = ["subvol=/nix" "compress=zstd" "noatime"];
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-uuid/D5CD-12F4";
+      fsType = "vfat";
+    };
   };
 
   swapDevices = [{device = "/dev/disk/by-uuid/8cb7bcbe-38c1-447d-a30e-93cb24875bbf";}];
-
-  boot.initrd.luks.devices.root = {
-    device = "/dev/disk/by-uuid/2e0eeb5d-bd42-471b-93c0-768b12d9b66e";
-    preLVM = true;
-  };
-
-  boot.loader = {
-    grub = {
-      enable = true;
-      efiSupport = true;
-      device = "nodev";
-      useOSProber = true;
-      enableCryptodisk = true;
-      saveDefault = true;
-    };
-    efi.canTouchEfiVariables = true;
-  };
 
   hardware.cpu.intel.updateMicrocode = true;
 }
