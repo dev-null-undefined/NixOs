bed261f74eaf3294da19a12a0f776d09a0d2a434
dev-null-undefined <martinkos007@gmail.com>
2023-12-16 15:01:50 +0100

    flake bump (which fixed jetbrains jdk)

--- modules/generated/nixos/packages/development/ides/jetbrains.nix
+++ modules/generated/nixos/packages/development/ides/jetbrains.nix
@@ -11,6 +11,8 @@
   ];
   plugins' = [
     "github-copilot"
+    "nixidea"
+    "csv-editor"
   ];
   ides-with-plugins' = builtins.map (ide: pkgs.master.jetbrains.plugins.addPlugins ide plugins') ides';
 in {
@@ -18,5 +20,5 @@ in {
   environment.systemPackages =
     ides'
     ++ (with pkgs.master;
-        (with jetbrains; [pkgs.stable.jetbrains.jdk gateway]) ++ [android-studio]);
+        (with jetbrains; [gateway]) ++ [android-studio]);
 }
