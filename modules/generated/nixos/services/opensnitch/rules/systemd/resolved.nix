9b264a6a5227588a3f6a0b59eb826909ec4aa7e1
dev-null-undefined <martinkos007@gmail.com>
2024-02-20 18:54:10 +0100

    opensnitch more specific rules

--- modules/generated/nixos/services/opensnitch/rules/systemd/resolved.nix
+++ modules/generated/nixos/services/opensnitch/rules/systemd/resolved.nix
@@ -9,11 +9,30 @@
       enabled = true;
       action = "allow";
       duration = "always";
-      operator = {
-        type = "simple";
-        sensitive = false;
-        operand = "process.path";
-        data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-resolved";
+      operator = rec {
+        type = "list";
+        operand = "list";
+        data = builtins.toJSON list;
+        list = [
+          {
+            type = "simple";
+            sensitive = false;
+            operand = "process.path";
+            data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-resolved";
+          }
+          {
+            type = "regexp";
+            operand = "dest.ip";
+            sensitive = false;
+            data = "^(1\\.1\\.1\\.1|1\\.0\\.0\\.1)$";
+          }
+          {
+            type = "simple";
+            operand = "dest.port";
+            sensitive = false;
+            data = "853";
+          }
+        ];
       };
     };
   };
--- modules/generated/nixos/services/opensnitch/rules/systemd/timesyncd.nix
+++ modules/generated/nixos/services/opensnitch/rules/systemd/timesyncd.nix
@@ -9,11 +9,30 @@
       enabled = true;
       action = "allow";
       duration = "always";
-      operator = {
-        type = "simple";
-        sensitive = false;
-        operand = "process.path";
-        data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-timesyncd";
+      operator = rec {
+        type = "list";
+        operand = "list";
+        data = builtins.toJSON list;
+        list = [
+          {
+            type = "simple";
+            sensitive = false;
+            operand = "process.path";
+            data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-timesyncd";
+          }
+          {
+            type = "regexp";
+            operand = "dest.host";
+            sensitive = false;
+            data = "^.*\\.nixos\\.pool\\.ntp\\.org$";
+          }
+          {
+            type = "simple";
+            operand = "dest.port";
+            sensitive = false;
+            data = "123";
+          }
+        ];
       };
     };
   };
