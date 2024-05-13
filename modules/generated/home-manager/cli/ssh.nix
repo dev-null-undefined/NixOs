1dbbf1d910a33b710377033f516559cbf894d34c
dev-null-undefined <martinkos007@gmail.com>
2024-05-13 18:58:19 +0200

    ssh work config

--- modules/generated/home-manager/cli/ssh.nix
+++ modules/generated/home-manager/cli/ssh.nix
@@ -42,6 +42,34 @@
           "PubkeyAcceptedKeyTypes" = "+ssh-rsa";
         };
       };
+
+      "cdn-dev-*" = {
+        user = "martin.kos";
+      };
+      "cdn-dev-ams-1" = {
+        hostname = "edge-mc-ams-dev-1-185-152-65-89.cdn77.com";
+      };
+      "cdn-dev-ams-2" = {
+        hostname = "edge-mc-ams-dev-2-185-152-65-22.cdn77.com";
+      };
+      "cdn-dev-fra-3" = {
+        hostname = "edge-mc-fra-dev-3-185-152-65-42.cdn77.com";
+      };
+      "cdn-dev-fra-4" = {
+        hostname = "edge-mc-fra-dev-4-185-152-65-43.cdn77.com";
+      };
+      "cdn-dev-prg-5" = {
+        hostname = "edge-mc-prg-dev-5-185-152-65-88.cdn77.com";
+      };
+      "cdn-dev-prg-6" = {
+        hostname = "edge-mc-prg-dev-6-185-152-65-83.cdn77.com";
+      };
+      "cdn-dev-prg-7" = {
+        hostname = "edge-mc-prg-dev-7-185-152-65-84.cdn77.com";
+      };
+      "cdn-dev-prg-8" = {
+        hostname = "edge-mc-prg-dev-8-45-136-152-18.cdn77.com";
+      };
     };
   };
 }
