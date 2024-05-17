cd37fe9b0c05fdddc567bd2380a4ae7fb9536e50
dev-null-undefined <martinkos007@gmail.com>
2024-05-17 15:46:31 +0200

    git work config

--- modules/generated/home-manager/cli/git.nix
+++ modules/generated/home-manager/cli/git.nix
@@ -7,6 +7,15 @@
       l = "log --color --pretty=format:'%Cred%h%Creset -%C(bold yellow)%d%Creset %s %Cgreen(%cr) %Cblue%an %C(bold blue)<%ae>%Creset %C(dim cyan)%G?' --abbrev-commit --reverse";
       ld = "ld = log -p -1";
     };
+    includes = [
+      {
+        condition = "gitdir:~/Work/CDN77/";
+        #path = "~/.config/git/config-work";
+        contents = {
+          user.name = "Martin Kos";
+        };
+      }
+    ];
     userName = "dev-null-undefined";
     userEmail = "martinkos007@gmail.com";
     extraConfig = {
