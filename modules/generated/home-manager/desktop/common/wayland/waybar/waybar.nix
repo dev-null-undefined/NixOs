62f7ce97e8c21a5e79d5ee9c491a299d4267e1d8
dev-null-undefined <martinkos007@gmail.com>
2024-05-17 15:47:11 +0200

    fixed waybar date

--- modules/generated/home-manager/desktop/common/wayland/waybar/waybar.nix
+++ modules/generated/home-manager/desktop/common/wayland/waybar/waybar.nix
@@ -86,10 +86,26 @@ in {
           separate-outputs = true;
         };
         clock = {
-          format = "{: %R   %d/%m}";
-          tooltip-format = ''
-            <big>{:%Y %B}</big>
-            <tt><small>{calendar}</small></tt>'';
+          format = "{:%H:%M}  ";
+          format-alt = "{:%d-%m-%Y (%R)}  ";
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+          calendar = {
+            mode = "year";
+            mode-mon-col = 3;
+            weeks-pos = "right";
+            on-scroll = 1;
+            on-click-right = "mode";
+            format = {
+              months = "<span color='#ffead3'><b>{}</b></span>";
+              days = "<span color='#ecc6d9'><b>{}</b></span>";
+              weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+              today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
+          actions = {
+            on-click-right = "mode";
+          };
           on-click = calendar;
         };
         cpu = {
