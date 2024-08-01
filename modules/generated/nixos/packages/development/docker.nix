d43e97970ec97578c6d0ea53423fc4d27f037da5
dev-null-undefined <martinkos007@gmail.com>
2024-08-01 13:55:25 +0200

    docker use rootless setup with setSocketVariable enabled

--- modules/generated/nixos/packages/development/docker.nix
+++ modules/generated/nixos/packages/development/docker.nix
@@ -2,6 +2,11 @@
   virtualisation.docker = {
     enable = true;
     enableOnBoot = false;
+
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
   };
   environment.systemPackages = with pkgs; [
     docker-compose
