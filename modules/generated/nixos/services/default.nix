73f5690806b7a2e2271c0f9a411f3869bd894f9d
dev-null-undefined <martinkos007@gmail.com>
2024-11-27 14:13:41 +0100

    tailscale init

--- modules/generated/nixos/services/default.nix
+++ modules/generated/nixos/services/default.nix
@@ -1,3 +1,6 @@
 {lib, ...}: {
-  generated.services.acme.enable = lib.mkDefault true;
+  generated.services = {
+    acme.enable = lib.mkDefault true;
+    tailscale.enable = lib.mkDefault true;
+  };
 }
