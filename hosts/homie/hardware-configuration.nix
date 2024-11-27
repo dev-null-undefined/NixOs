d75b905c6f403c459652e4221fb0b0c37003258d
dev-null-undefined <martinkos007@gmail.com>
2024-11-27 14:14:46 +0100

    homie updated hardware conf

--- hosts/homie/hardware-configuration.nix
+++ hosts/homie/hardware-configuration.nix
@@ -5,11 +5,19 @@
   modulesPath,
   ...
 }: {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
+  imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
-  boot.initrd.availableKernelModules = ["uhci_hcd" "ehci_pci" "ata_piix" "ahci" "xhci_pci" "firewire_ohci" "usbhid" "usb_storage" "sd_mod"];
+  boot.initrd.availableKernelModules = [
+    "uhci_hcd"
+    "ehci_pci"
+    "ata_piix"
+    "ahci"
+    "xhci_pci"
+    "firewire_ohci"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
@@ -18,5 +26,20 @@
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  boot.kernel.sysctl = {
+    # if you use ipv4, this is all you need
+    "net.ipv4.conf.all.forwarding" = true;
+
+    # If you want to use it for ipv6
+    "net.ipv6.conf.all.forwarding" = true;
+
+    # source: https://github.com/mdlayher/homelab/blob/master/nixos/routnerr-2/configuration.nix#L52
+    # By default, not automatically configure any IPv6 addresses.
+    "net.ipv6.conf.all.accept_ra" = 0;
+    "net.ipv6.conf.all.autoconf" = 0;
+    "net.ipv6.conf.all.use_tempaddr" = 0;
+  };
+
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
--- hosts/homie/hardware-partitions.nix
+++ hosts/homie/hardware-partitions.nix
@@ -1,32 +1,34 @@
-{...}: {
+{...}: let
+  root = "18527a28-5247-4d36-b8d8-63b086317b81";
+in {
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/cc0038f9-46f9-47d9-a9b8-90e3a0f08536";
+    device = "/dev/disk/by-uuid/${root}";
     fsType = "btrfs";
     options = ["subvol=root" "compress=zstd"];
   };
 
   fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/cc0038f9-46f9-47d9-a9b8-90e3a0f08536";
+    device = "/dev/disk/by-uuid/${root}";
     fsType = "btrfs";
     options = ["subvol=home" "compress=zstd"];
   };
 
   fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/cc0038f9-46f9-47d9-a9b8-90e3a0f08536";
+    device = "/dev/disk/by-uuid/${root}";
     fsType = "btrfs";
     options = ["subvol=nix" "noatime" "compress=zstd"];
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/D764-0C6B";
-    fsType = "vfat";
+  fileSystems."/var/data" = {
+    device = "/dev/disk/by-uuid/640bd80d-0c99-447d-81b9-8ec168ea43f8";
+    fsType = "btrfs";
+    options = ["subvol=data" "noatime" "compress=zstd"];
   };
 
-  swapDevices = [];
+  swapDevices = [{device = "/dev/disk/by-uuid/b430b9b3-5bb7-435f-890e-b3f3695a8256";}];
 
   boot.loader.grub = {
     enable = true;
-    version = 2;
-    device = "/dev/sda";
+    device = "/dev/disk/by-id/wwn-0x5002538e3054aa55";
   };
 }
