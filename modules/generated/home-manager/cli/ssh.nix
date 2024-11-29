538ca3ae523a446cbad72d5577787c26a6c7c28d
dev-null-undefined <martinkos007@gmail.com>
2024-11-29 18:39:14 +0100

    use martin.kos for all cdn servers

--- modules/generated/home-manager/cli/ssh.nix
+++ modules/generated/home-manager/cli/ssh.nix
@@ -49,10 +49,8 @@
           "PubkeyAcceptedKeyTypes" = "+ssh-rsa";
         };
       };
-
-      "cdn-*" = {
-        user = "martin.kos";
-      };
+      "*.cdn77.eu" = {user = "martin.kos";};
+      "*.cdn77.com" = {user = "martin.kos";};
       "cdn-dev-ams-1" = {
         hostname = "edge-mc-ams-dev-1-185-152-65-89.cdn77.com";
       };
