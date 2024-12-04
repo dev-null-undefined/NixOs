fbf3247cf3b286b2f11a8715d0baef416d15a783
dev-null-undefined <martinkos007@gmail.com>
2024-12-04 11:02:35 +0100

    moved secrets into persistant folder

--- modules/generated/nixos/network-profiles/default.nix
+++ modules/generated/nixos/network-profiles/default.nix
@@ -3,5 +3,5 @@
     vpn.enable = lib.mkDefault true;
     wifi.enable = lib.mkDefault true;
   };
-  networking.networkmanager.ensureProfiles.environmentFiles = ["/run/secrets/network-manager.env"];
+  networking.networkmanager.ensureProfiles.environmentFiles = ["/var/secrets/network-manager.env"];
 }
