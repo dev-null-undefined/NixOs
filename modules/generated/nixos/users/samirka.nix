74e3c30861e5314c4de8060927b8c4498f1d2b00
dev-null-undefined <martinkos007@gmail.com>
2024-12-11 17:48:01 +0100

    samirka database a apka

--- modules/generated/nixos/users/samirka.nix
+++ modules/generated/nixos/users/samirka.nix
@@ -5,6 +5,7 @@
   ...
 }: let
   sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBt3RVg6B5EfYY9uS14yfm78VEI1wUhr4sv2FYbxf6JS devnull@Devs-MacBook-Pro.local";
+  dbName = "bookheaven";
 in {
   users.users.samirka = {
     isNormalUser = true;
@@ -23,6 +24,21 @@ in {
     useDefaultShell = false;
     openssh.authorizedKeys.keys = [sshKey];
   };
+
+  services.mysql = {
+    ensureDatabases = [dbName];
+    ensureUsers = [
+      {
+        name = dbName;
+        ensurePermissions = {"${dbName}.*" = "ALL PRIVILEGES";};
+      }
+    ];
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    6969 # UwU acka apka
+    3306
+  ];
 
   home-manager.users = self.lib'.internal.mkHomeNixOsUser "samirka" [];
 }
