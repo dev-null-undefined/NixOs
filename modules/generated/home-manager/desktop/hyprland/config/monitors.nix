71049b8abbf801d221225a213a968d825ed5842e
dev-null-undefined <martinkos007@gmail.com>
2025-02-27 20:40:50 +0100

    monitor setup

--- modules/generated/home-manager/desktop/hyprland/config/monitors.nix
+++ modules/generated/home-manager/desktop/hyprland/config/monitors.nix
@@ -3,7 +3,8 @@
     monitor = [
       "eDP-1,3840x2400@90.0,0x0,2"
       ",preferred,auto,1"
-      "desc:HP Inc. HP X34 6CM14806T6,highrr,auto,1"
+      "desc:HP Inc. HP X34 6CM14806T6,highrr,auto-up,1"
+      "desc:Microstep MPG321UX OLED 0x01010101,highrr,auto,1"
     ];
     "render:explicit_sync" = 0;
   };
