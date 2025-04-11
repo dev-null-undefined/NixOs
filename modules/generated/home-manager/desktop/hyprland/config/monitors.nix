e805d9bc898d39744802b2628465f3af3ddb37e0
dev-null-undefined <martinkos007@gmail.com>
2025-04-11 13:49:13 +0200

    finally setuped monitors

--- modules/generated/home-manager/desktop/hyprland/config/monitors.nix
+++ modules/generated/home-manager/desktop/hyprland/config/monitors.nix
@@ -1,10 +1,10 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "eDP-1,3840x2400@90.0,0x0,2"
       ",preferred,auto,1"
-      "desc:HP Inc. HP X34 6CM14806T6,highrr,auto-up,1"
-      "desc:Microstep MPG321UX OLED 0x01010101,highrr,auto,1"
+      "desc:HP Inc. HP X34 6CM14806T6,highrr,700x0,1"
+      "desc:Microstep MPG321UX OLED 0x01010101,highrr,0x1440,1"
+      "desc:HP Inc. HP 24fh 3CM0330W0G,preferred,3840x1440,1"
     ];
     "render:explicit_sync" = 0;
   };
