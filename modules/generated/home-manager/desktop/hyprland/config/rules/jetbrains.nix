e8fe83c3639c08061e6bab0a5c17ad0f0b7c487d
dev-null-undefined <martinkos007@gmail.com>
2025-06-24 09:14:21 +0200

    jetbrains rules hyprland ...

--- modules/generated/home-manager/desktop/hyprland/config/rules/jetbrains.nix
+++ modules/generated/home-manager/desktop/hyprland/config/rules/jetbrains.nix
@@ -2,7 +2,6 @@
   wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
       "tag +jb, class:^jetbrains-.+$,floating:1"
-      "stayfocused, tag:jb"
       "noinitialfocus, tag:jb"
     ];
   };
