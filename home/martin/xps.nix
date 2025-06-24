ed1f774314749bccd1a831405fc73e467121018c
dev-null-undefined <martinkos007@gmail.com>
2025-06-24 09:13:21 +0200

    moved monitors

--- home/martin/xps.nix
+++ home/martin/xps.nix
@@ -3,8 +3,8 @@
     monitor = lib.mkForce [
       ",preferred,auto,1"
       "desc:BNQ BenQ PD3205U 59P01340019,preferred,0x0,1,transform,1"
-      "desc:BNQ BenQ PD3205U GBP00096019,preferred,2160x1100,1"
-      "desc:Samsung Display Corp. 0x4196,preferred,6000x2464,2"
+      "desc:BNQ BenQ PD3205U GBP00096019,preferred,2160x785,1"
+      "desc:Samsung Display Corp. 0x4196,preferred,6000x1400,2"
     ];
     workspace = [
       "11, monitor:desc:BNQ BenQ PD3205U 59P01340019, default:true"
