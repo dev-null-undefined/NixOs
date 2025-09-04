ef467e7cce10d2410c7faf8d1d83926b81419646
dev-null-undefined <martinkos007@gmail.com>
2025-09-04 19:12:43 +0200

    jetbrains from stable

--- modules/generated/nixos/packages/development/ides/jetbrains.nix
+++ modules/generated/nixos/packages/development/ides/jetbrains.nix
@@ -1,28 +1,5 @@
-{
-  pkgs,
-  config,
-  ...
-}: let
-  jetbrains =
-    (
-      import
-      (fetchTarball {
-        # Date: 20250713
-
-        # https://github.com/NixOS/nixpkgs/issues/425328
-
-        url = "https://github.com/NixOS/nixpkgs/tarball/9807714d6944a957c2e036f84b0ff8caf9930bc0";
-
-        sha256 = "sha256:1g9qc3n5zx16h129dqs5ixfrsff0dsws9lixfja94r208fq9219g";
-      })
-      {
-        config = {allowUnfree = true;};
-
-        localSystem = {
-          system = "x86_64-linux";
-        };
-      }
-    ).jetbrains;
+{pkgs, ...}: let
+  inherit (pkgs.stable) jetbrains;
 
   ides' = with jetbrains; [
     idea-ultimate
@@ -34,12 +11,18 @@
     datagrip
     rust-rover
   ];
-  plugins' = ["github-copilot" "nixidea" "csv-editor"];
-  ides-with-plugins' =
-    builtins.map (ide: jetbrains.plugins.addPlugins ide plugins') ides';
+  plugins' = [
+    "github-copilot"
+    "nixidea"
+    "csv-editor"
+  ];
+  ides-with-plugins' = builtins.map (ide: jetbrains.plugins.addPlugins ide plugins') ides';
 in {
   #environment.systemPackages = ides-with-plugins' ++ [jdk gateway];
   environment.systemPackages =
     ides'
-    ++ [jetbrains.gateway pkgs.android-studio];
+    ++ [
+      jetbrains.gateway
+      pkgs.android-studio
+    ];
 }
