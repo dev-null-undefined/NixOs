74e8108af8f55eb35acdb781690db7ff26bd1481
dev-null-undefined <martinkos007@gmail.com>
2025-09-13 10:43:48 +0200

    honey new monitor setup

--- home/martin/honey.nix
+++ home/martin/honey.nix
@@ -3,14 +3,16 @@
   wayland.windowManager.hyprland.settings = {
     monitor = lib.mkForce [
       ",preferred,auto,1"
-      "desc:HP Inc. HP X34 6CM14806T6,highrr,700x0,1"
-      "desc:Microstep MPG321UX OLED 0x01010101,highrr,0x1440,1"
+      "desc:HP Inc. HP X34 6CM14806T6,3440x1440@165,400x0,1"
+      "desc:AOC 27G2G4 GYGLCHA305056,1920x1080@144,3840x1000,1,transform,1"
+      "desc:Microstep MPG321UX OLED 0x01010101,3840x2160@240,0x1440,1"
       "desc:HP Inc. HP 24fh 3CM0330W0G,preferred,3840x1440,1"
     ];
     workspace = [
       "1, monitor:desc:Microstep MPG321UX OLED 0x01010101, default:true"
       "11, monitor:desc:HP Inc. HP X34 6CM14806T6, default:true"
-      "4, monitor:desc:HP Inc. HP 24fh 3CM0330W0G, default:true"
+      "9, monitor:desc:AOC 27G2G4 GYGLCHA305056, default:true"
+      # "4, monitor:desc:HP Inc. HP 24fh 3CM0330W0G, default:true"
     ];
   };
 }
