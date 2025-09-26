ab0a5b667b1069de7b2d75f5e0ace0c0ada5b393
dev-null-undefined <martinkos007@gmail.com>
2025-09-26 13:10:04 +0200

    added zen browser

--- modules/generated/nixos/packages/gui/browsers/default.nix
+++ modules/generated/nixos/packages/gui/browsers/default.nix
@@ -1,6 +1,6 @@
 {lib, ...}: {
   generated.packages.gui.browsers =
     lib.attrsets.genAttrs
-    ["firefox" "chromium"]
+    ["firefox" "chromium" "zen"]
     (_: {enable = lib.mkDefault true;});
 }
