da42e720de29c468c04ad84448b87372e5b0bb44
dev-null-undefined <martinkos007@gmail.com>
2025-10-04 07:40:54 +0200

    tailscale restart after suspend to fix DNS issue

--- modules/generated/nixos/services/tailscale.nix
+++ modules/generated/nixos/services/tailscale.nix
@@ -1,1 +1,17 @@
-{services.tailscale.enable = true;}
+{config, ...}: {
+  services.tailscale.enable = true;
+
+  # Systemd service to restart Tailscale after waking up from suspend
+  systemd.services.tailscale-restart-after-suspend = {
+    description = "Restart Tailscale after waking up";
+    after = ["suspend.target"];
+    wantedBy = ["suspend.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ''
+        ${config.services.tailscale.package}/bin/tailscale down && sleep 0.1 && ${config.services.tailscale.package}/bin/tailscale up
+      '';
+      User = "root";
+    };
+  };
+}
