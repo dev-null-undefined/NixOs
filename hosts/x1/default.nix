0bfc3b1be2ef58cef58847a9d9c3db82e860712a
dev-null-undefined <martinkos007@gmail.com>
2025-10-07 09:41:00 +0200

    removed tlp configuration as it's already part of hyprland config

--- hosts/x1/default.nix
+++ hosts/x1/default.nix
@@ -60,13 +60,6 @@
     ];
   };
 
-  services.tlp.extraConfig = ''
-    START_CHARGE_THRESH_BAT0=75
-    STOP_CHARGE_THRESH_BAT0=85
-    CPU_SCALING_GOVERNOR_ON_BAT=powersave
-    ENERGY_PERF_POLICY_ON_BAT=powersave
-  '';
-
   networking.modemmanager.enable = true;
 
   networking.modemmanager.fccUnlockScripts = [
