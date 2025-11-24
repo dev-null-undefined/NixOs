df817151c82ea9bcd332a2342b9cab594ee562c2
dev-null-undefined <martinkos007@gmail.com>
2025-11-24 09:30:58 +0100

    changed monitor settings

--- home/martin/xps.nix
+++ home/martin/xps.nix
@@ -2,9 +2,9 @@
   wayland.windowManager.hyprland.settings = {
     monitor = lib.mkForce [
       ",preferred,auto,1"
-      "desc:BNQ BenQ PD3205U 59P01340019,preferred,0x0,1,transform,1"
-      "desc:BNQ BenQ PD3205U GBP00096019,preferred,2160x785,1"
-      "desc:Samsung Display Corp. 0x4196,preferred,6000x1400,2"
+      "desc:BNQ BenQ PD3205U 59P01340019,preferred,0x0,1.5,transform,1"
+      "desc:BNQ BenQ PD3205U GBP00096019,preferred,1440x590,1.5"
+      "desc:Samsung Display Corp. 0x4196,preferred,4000x750,2"
     ];
     workspace = [
       "11, monitor:desc:BNQ BenQ PD3205U 59P01340019, default:true"
