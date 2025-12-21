08a3421fe75323bb1475dc0a9c00e15c0dc81430
dev-null-undefined <martinkos007@gmail.com>
2025-12-21 11:17:18 +0100

    flameshot rules

---
+++ modules/generated/home-manager/desktop/hyprland/config/rules/flameshot.nix
@@ -0,0 +1,10 @@
+{
+  wayland.windowManager.hyprland.settings = {
+    windowrulev2 = [
+      "move 0 0,match:class (flameshot),match:title (flameshot)"
+      "pin on,match:class (flameshot),match:title (flameshot)"
+      "fullscreenstate on,match:class (flameshot),match:title (flameshot)"
+      "float on,match:class (flameshot),match:title (flameshot)"
+    ];
+  };
+}
