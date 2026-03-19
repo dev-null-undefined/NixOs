93f4cf0f298b855ae9ddb5b0d2301eab80860837
dev-null-undefined <martinkos007@gmail.com>
2026-03-19 18:03:38 +0100

    add SOPS-encrypted secret key for Grafana security setting
    Grafana removed the default value for services.grafana.settings.security.secret_key,
    requiring an explicit secret. Uses file-provider to read from a SOPS-managed secret.

--- hosts/homie/grafana.nix
+++ hosts/homie/grafana.nix
@@ -1,4 +1,14 @@
-{config, ...}: {
+{
+  config,
+  self,
+  ...
+}: {
+  sops.secrets."grafana-secret-key" = {
+    sopsFile = self.outPath + "/secrets/grafana-secret-key";
+    format = "binary";
+    owner = "grafana";
+  };
+
   services.grafana = {
     enable = true;
     settings = {
@@ -7,6 +17,7 @@
         http_port = 3000;
         root_url = "https://grafana.dev-null.me";
       };
+      security.secret_key = "$__file{${config.sops.secrets."grafana-secret-key".path}}";
     };
   };
   services.nginx.virtualHosts."grafana.dev-null.me" = {
