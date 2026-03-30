03ce8e967dd7826d31f0c23128b6b2bdb955f1c9
dev-null-undefined <martinkos007@gmail.com>
2026-03-30 15:01:39 +0200

    add hypridle module with lock and sleep handling

---
+++ modules/generated/home-manager/desktop/hyprland/hypridle.nix
@@ -0,0 +1,12 @@
+{...}: {
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+    };
+  };
+}
