4ce43c4b1194ce760d7b3aa7c93c47b2f3439fc6
dev-null-undefined <martinkos007@gmail.com>
2026-03-30 15:19:54 +0200

    swap ISO §/± key with grave accent/tilde on macOS keyboard

--- modules/generated/darwin/system/keyboard.nix
+++ modules/generated/darwin/system/keyboard.nix
@@ -1,3 +1,16 @@
 {
-  system.keyboard.enableKeyMapping = true;
+  system.keyboard = {
+    enableKeyMapping = true;
+    # Swap ISO § key (next to left shift) with Grave Accent/Tilde (left of 1)
+    userKeyMapping = [
+      {
+        HIDKeyboardModifierMappingSrc = 30064771172; # 0x700000064 - Non-US Backslash (ISO §/±)
+        HIDKeyboardModifierMappingDst = 30064771125; # 0x700000035 - Grave Accent/Tilde
+      }
+      {
+        HIDKeyboardModifierMappingSrc = 30064771125; # 0x700000035 - Grave Accent/Tilde
+        HIDKeyboardModifierMappingDst = 30064771172; # 0x700000064 - Non-US Backslash (ISO §/±)
+      }
+    ];
+  };
 }
