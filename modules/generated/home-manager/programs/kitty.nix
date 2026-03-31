7c70f1c971b237bba9d1d1583abb4752a58c5d8f
dev-null-undefined <martinkos007@gmail.com>
2026-03-31 02:11:51 +0200

    add kitty font configuration

--- modules/generated/home-manager/programs/kitty.nix
+++ modules/generated/home-manager/programs/kitty.nix
@@ -2,6 +2,8 @@
   programs.kitty = {
     enable = true;
     settings = {
+      font_family = "MesloLGS Nerd Font Mono";
+      font_size = 13;
       scrollback_lines = 10000;
       enable_audio_bell = false;
       confirm_os_window_close = 0;
