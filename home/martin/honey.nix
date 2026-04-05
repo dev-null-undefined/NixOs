ce0ab86559137de6c63802b9bad2224de8d4e591
dev-null-undefined <martinkos007@gmail.com>
2026-04-05 14:39:58 +0200

    update honey monitor layout: 4x 4K@120Hz with MSI OLED on top
    Replace old HP/AOC monitor config with new 4-monitor setup:
    MSI 32" OLED (top center), ASUS PG27UCDM (bottom center),
    2x Dell U2725QE (bottom left/right). All at 4K@120Hz due to
    RTX 4070 Ti SUPER display engine bandwidth limit.

--- home/martin/honey.nix
+++ home/martin/honey.nix
@@ -3,16 +3,20 @@
   wayland.windowManager.hyprland.settings = {
     monitor = lib.mkForce [
       ",preferred,auto,1"
-      # "desc:HP Inc. HP X34 6CM14806T6,3440x1440@165,0x0,1.25"
-      "desc:AOC 27G2G4 GYGLCHA305056,1920x1080@144,2560x0,1"
-      "desc:Microstep MPG321UX OLED 0x01010101,3840x2160@240,0x0,1.5"
-      # "desc:HP Inc. HP 24fh 3CM0330W0G,preferred,3840x1440,1"
+      # top center: MSI 32" OLED
+      "desc:Microstep MPG321UX OLED 0x01010101,3840x2160@120,4480x0,1.5"
+      # bottom left: Dell 27"
+      "desc:Dell Inc. DELL U2725QE 4DC0JF4,3840x2160@120,0x1440,1"
+      # bottom center: ASUS 27"
+      "desc:ASUSTek COMPUTER INC PG27UCDM TALMAS004976,3840x2160@120,3840x1440,1"
+      # bottom right: Dell 27"
+      "desc:Dell Inc. DELL U2725QE 79C0JF4,3840x2160@120,7680x1440,1"
     ];
     workspace = [
       "1, monitor:desc:Microstep MPG321UX OLED 0x01010101, default:true"
-      "11, monitor:desc:HP Inc. HP X34 6CM14806T6, default:true"
-      "9, monitor:desc:AOC 27G2G4 GYGLCHA305056, default:true"
-      # "4, monitor:desc:HP Inc. HP 24fh 3CM0330W0G, default:true"
+      "2, monitor:desc:ASUSTek COMPUTER INC PG27UCDM TALMAS004976, default:true"
+      "3, monitor:desc:Dell Inc. DELL U2725QE 4DC0JF4, default:true"
+      "4, monitor:desc:Dell Inc. DELL U2725QE 79C0JF4, default:true"
     ];
   };
 }
