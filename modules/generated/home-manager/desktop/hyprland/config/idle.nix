2e0e7543501f56cd7ebff2cb0254601de7ad9552
dev-null-undefined <martinkos007@gmail.com>
2026-04-08 18:31:18 +0200

    switch idle timeout from monitor disable to dpms off/on

--- modules/generated/home-manager/desktop/hyprland/config/idle.nix
+++ modules/generated/home-manager/desktop/hyprland/config/idle.nix
@@ -1,18 +1,10 @@
 {config, ...}: let
-  # Monitors that need disabling instead of DPMS (NVIDIA HDMI FRL link training
-  # fails on DPMS resume, freezing the compositor). Using monitor disable/enable
-  # does a full modeset which avoids the FRL re-negotiation bug.
   monitors =
     builtins.filter (m: m != ",preferred,auto,1")
     config.wayland.windowManager.hyprland.settings.monitor;
-  disableAll = builtins.concatStringsSep " ; " (map (m:
-    let
-      desc = builtins.head (builtins.split "," m);
-    in "hyprctl keyword monitor \"${desc},disable\"")
-  monitors);
-  enableAll = builtins.concatStringsSep " ; " (map (m:
-    "hyprctl keyword monitor \"${m}\"")
-  monitors);
+  enableAll =
+    builtins.concatStringsSep " ; " (map (m: "hyprctl keyword monitor \"${m}\"")
+      monitors);
 in {
   services.hypridle = {
     enable = true;
@@ -32,8 +24,8 @@ in {
         # Turn off monitors after 15 minutes (OLED burn-in protection)
         {
           timeout = 900;
-          on-timeout = disableAll;
-          on-resume = enableAll;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
         }
       ];
     };
