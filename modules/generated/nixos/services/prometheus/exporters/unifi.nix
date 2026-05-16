4b7bb73e4a0e2fa0cc0a25f58cdf5e5171b8e477
dev-null-undefined <martinkos007@gmail.com>
2026-05-16 21:09:28 +0200

    unifi exporter: move prometheus password to sops

--- modules/generated/nixos/services/prometheus/exporters/unifi.nix
+++ modules/generated/nixos/services/prometheus/exporters/unifi.nix
@@ -1,10 +1,20 @@
-{config, ...}: {
+{
+  config,
+  self,
+  ...
+}: {
+  sops.secrets."unifi-prometheus-pass" = {
+    sopsFile = self.outPath + "/secrets/unifi-prometheus-pass";
+    format = "binary";
+    owner = "unpoller-exporter";
+  };
+
   services.prometheus.exporters.unpoller = {
     enable = true;
     controllers = [
       {
         user = "prometheus";
-        pass = "/var/unifi-prometheus-pass";
+        pass = config.sops.secrets."unifi-prometheus-pass".path;
         url = "https://127.0.0.1:${toString config.registry.services.unifi.port}";
         verify_ssl = false;
       }
