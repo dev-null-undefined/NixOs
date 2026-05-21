2f548efd6813d23cbc006d1d5fb687a5d8811124
dev-null-undefined <martinkos007@gmail.com>
2026-05-21 13:03:03 +0200

    hyprlock: add keyboard layout label below input field

--- modules/generated/home-manager/desktop/hyprland/hyprlock.nix
+++ modules/generated/home-manager/desktop/hyprland/hyprlock.nix
@@ -1,4 +1,14 @@
-{...}: {
+{pkgs, ...}: let
+  layoutScript = pkgs.writeShellScriptBin "hyprlock-layout" ''
+    layout=$(${pkgs.hyprland}/bin/hyprctl devices -j 2>/dev/null \
+      | ${pkgs.jq}/bin/jq -r '[.keyboards[] | select(.main==true)] | first | .active_keymap // "??"')
+    case "$layout" in
+      "English (US)") echo "󰌌 US" ;;
+      "Czech"*) echo "󰌌 CZ" ;;
+      *) echo "󰌌 $layout" ;;
+    esac
+  '';
+in {
   programs.hyprlock = {
     enable = true;
     settings = {
@@ -53,6 +63,15 @@
           font_size = 24;
           font_family = "Inter";
           position = "0, 70";
+          halign = "center";
+          valign = "center";
+        }
+        {
+          text = "cmd[update:500] ${layoutScript}/bin/hyprlock-layout";
+          color = "rgb(fab387)";
+          font_size = 18;
+          font_family = "Cartograph CF Nerd Font";
+          position = "0, -110";
           halign = "center";
           valign = "center";
         }
