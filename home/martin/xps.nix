de90bc572a83521fa8dfabb23a65b045cb5e04ff
dev-null-undefined <martinkos007@gmail.com>
2026-06-02 18:10:00 +0200

    xps: swap benq pd3205u monitor positions

--- home/martin/xps.nix
+++ home/martin/xps.nix
@@ -3,13 +3,13 @@
   wayland.windowManager.hyprland.settings = {
     monitor = lib.mkForce [
       ",preferred,auto,1"
-      "desc:BNQ BenQ PD3205U 59P01340019,preferred,0x0,1.5,transform,1"
-      "desc:BNQ BenQ PD3205U GBP00096019,preferred,1440x590,1.5"
+      "desc:BNQ BenQ PD3205U GBP00096019,preferred,0x0,1.5,transform,1"
+      "desc:BNQ BenQ PD3205U 59P01340019,preferred,1440x590,1.5"
       "desc:Samsung Display Corp. 0x4196,preferred,4000x750,2"
     ];
     workspace = [
-      "11, monitor:desc:BNQ BenQ PD3205U 59P01340019, default:true"
-      "1, monitor:desc:BNQ BenQ PD3205U GBP00096019, default:true"
+      "11, monitor:desc:BNQ BenQ PD3205U GBP00096019, default:true"
+      "1, monitor:desc:BNQ BenQ PD3205U 59P01340019, default:true"
       "9, monitor:desc:Samsung Display Corp. 0x4196, default:true"
     ];
   };
